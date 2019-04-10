67ed10283494a0691446d2644f197e6b04e75495
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-04-10 15:19:07 +0530

    Add files via upload

--- beanbagfunct.js
+++ beanbagfunct.js
@@ -1,8 +1,6 @@
 var ans;
 module.exports = {
 
-	
-
 	on_off_funct : function(v)
 	{
 		if(v!=null && v.parameters && v.parameters.state && v.parameters["number-sequence"])
@@ -20,6 +18,31 @@ module.exports = {
 				else
 					{
 					ans = "sorry can you say again with room number";
+					}
+				return ans;
+	},
+	
+	operate_mode_funct : function(v)
+	{
+		if(v!=null && v.parameters && v.parameters.mode)
+						{
+							if(v.parameters.mode == "comfort")
+							{
+							ans = "ok ac running in comfort mode";
+							}
+							else if(v.parameters.mode == "away")
+							{
+							ans = "ok ac running in away mode";
+							}
+							else if(v.parameters.mode == "customise")
+							{
+							ans = "ok ac running in customise mode";
+							}
+							
+						}
+				else
+					{
+					ans = "sorry can you specify the valid mode";
 					}
 				return ans;
 	}
