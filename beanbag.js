9b09c7b211443d4f63a284a7d5889d29ea3914e0
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-04-10 11:18:43 +0530

    Add files via upload

---
+++ beanbag.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/res", function(req, res) {
+
+	var ans;
+
+	if(req.body.queryResult.intent.displayName == "on_off_state")
+		{
+			if(req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.state)
+				{
+					ans = "hello from api";
+				}
+		}
+		
+		
+	return res.json({
+			fulfillmentText: ans,
+			source: "beanbag"
+		  });
+		  
+}).listen(process.env.PORT||9879);
